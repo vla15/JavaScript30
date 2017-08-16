410208683f4a185823249787b071020f8cb1505f
Vincent La <vincela15@gmail.com>
2017-08-16 14:57:37 -0700

    updating array cardio day 2

---
+++ 07 - Array Cardio Day 2/index.js
@@ -0,0 +1,49 @@
+// ## Array Cardio Day 2
+
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+// Array.prototype.every() // is everyone 19 or older?
+
+const someone = people.some(person => {
+  return (2017 - person.year) > 19;
+});
+
+const all = people.every(person => {
+  return (2017 - person.year) > 19;
+});
+
+
+const findId = comments.find(comment => comment.id === 823423);
+
+const findIndex = comments.findIndex(comment => comment.id === 823423);
+
+// comments.splice(findIndex, 1);
+
+const newComments = [
+...comments.slice(0, findIndex),
+...comments.slice(findIndex + 1)
+];
+console.log(newComments);
+console.log(comments);
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
